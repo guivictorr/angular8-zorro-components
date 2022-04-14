2e9cb2299b03a78b49a8b1469c7683c440426b92
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-04-14 09:34:46 -0300

    centralizing the show more button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,26 @@ export class AppComponent {
       sobrenome: "Paulo",
       idade: "30",
       expand: false,
+      children: [
+        {
+          nome: "Maria",
+          sobrenome: "Antonia",
+          idade: "42",
+          expand: true,
+        },
+      ],
+    },
+    {
+      nome: "Jorge",
+      sobrenome: "Paulo",
+      idade: "30",
+      expand: false,
+    },
+    {
+      nome: "Jorge",
+      sobrenome: "Paulo",
+      idade: "30",
+      expand: false,
     },
   ];
 
